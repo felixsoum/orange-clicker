63fd7edd229c3fcc0e199932db06bff89b843080
felixsoum <felix@felixsoum.com>
2025-04-03 07:20:56 -0400

    Create build

---
+++ Assets/Scripts/OrangeButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OrangeButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
